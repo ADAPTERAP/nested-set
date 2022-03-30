af0bb0ad7564814708690c689ed80f26cc9e54d3
Nikkoz <Nikkoz@users.noreply.github.com>
2022-03-30 14:14:41 +0000

    Apply php-cs-fixer changes

--- src/Relations/BaseRelation.php
+++ src/Relations/BaseRelation.php
@@ -2,13 +2,13 @@
 
 namespace Adapterap\NestedSet\Relations;
 
-use Illuminate\Support\Arr;
+use Adapterap\NestedSet\Exceptions\NestedSetModelHasNoScope;
 use Adapterap\NestedSet\NestedSetModelTrait;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Adapterap\NestedSet\Exceptions\NestedSetModelHasNoScope;
+use Illuminate\Support\Arr;
 
 /**
  * Class BaseRelation.
